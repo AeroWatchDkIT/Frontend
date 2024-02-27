c173a233ea674da314e67eedc662200900f7d147
Vincent Arellano <67905295+VincentArellano@users.noreply.github.com>
2024-02-27 20:49:49 +0000

    Encrypt API calls

--- cabin-view-ui/src/services/palletStatus.ts
+++ cabin-view-ui/src/services/palletStatus.ts
@@ -2,22 +2,22 @@ import type { PalletStatuses } from "../types/palletStatus";
 
 export default class PalletStatusService {
   static async getAllData(): Promise<PalletStatuses[]> {
-    const apiUrl = '/PalletStatuses';
+    const apiUrl = import.meta.env.VITE_ALL_PALLET_STATUS;
     return await PalletStatusService.fetchData(apiUrl);
   }
 
   static async getDataBySearchTerm(searchTerm: string): Promise<PalletStatuses[]> {
-    const apiUrl = `/PalletStatuses/search?SearchTerm=${searchTerm.trim()}`;
+    const apiUrl = `${import.meta.env.VITE_PALLET_SEARCH}?SearchTerm=${searchTerm.trim()}`;
     return await PalletStatusService.fetchData(apiUrl);
   }
 
   static async getDataByPalletState(palletStatus: string): Promise<PalletStatuses[]> {
-    const apiUrl = `/PalletStatuses?filterTerm=${palletStatus.trim()}`;
+    const apiUrl = `${import.meta.env.VITE_PALLET_FILTER}${palletStatus.trim()}`;
     return await PalletStatusService.fetchData(apiUrl);
   }
 
   private static async fetchData(apiUrl: string): Promise<PalletStatuses[]> {
-    const baseApiUrl = 'https://localhost:7128';
+    const baseApiUrl = import.meta.env.VITE_DATABASE_HOST;
 
     try {
       const response = await fetch(`${baseApiUrl}${apiUrl}`, {
