b84a2bc4721738c3a22759d2a8654241c7238511
VincentArellano <vincentvarellano08@gmail.com>
2024-04-24 16:20:29 +0100

    Revert to previous commit

--- cabin-view-ui/src/components/MainPage.vue
+++ cabin-view-ui/src/components/MainPage.vue
@@ -1,28 +1,6 @@
 <template>
   <div class="main-container">
     <img :src="props.cameraFeedUrl" alt="Camera Feed" class="camera-feed" />
-
-    <div class="info-display">
-      <div
-        class="info-part pallet"
-        :class="{ 'tag-match': pallet.matching, 'no-tags': !pallet.text }"
-      >
-        <h3 v-if="pallet.matching">Tags Matching</h3>
-        <h3 v-else>Pallet Tag</h3>
-        <p>{{ pallet.text }}</p>
-        <small>Recognized at: {{ pallet.time }}</small>
-      </div>
-      <div
-        class="info-part shelf-tag"
-        :class="{ 'tag-match': place.matching, 'no-tags': !place.text }"
-      >
-        <h3 v-if="place.matching">Tags Matching</h3>
-        <h3 v-else>Shelf Tag</h3>
-        <p>{{ place.text }}</p>
-        <small>Recognized at: {{ place.time }}</small>
-      </div>
-    </div>
-
     <div class="leftside-buttons">
       <Button
         class="alert-button"
@@ -57,7 +35,6 @@
       </Button>
     </div>
     <DynamicDialog
-      v-if="alertModalVisible"
       class="alertTest"
       :pt="{
         content: {
@@ -69,7 +46,6 @@
           },
         },
       }"
-      @onHide="alertModalVisible = false"
     />
     <CameraInfoToast
       :recognition-time="props.recognitionTime"
@@ -86,86 +62,48 @@ import DynamicDialog from "primevue/dynamicdialog";
 import CameraInfoToast from "./CameraInfoToast.vue";
 import MissingPallet from "./MissingPallet.vue";
 import AlertModal from "./AlertModal.vue";
+import { useDialog } from "primevue/usedialog";
 import { ref, watch, toRef } from "vue";
 import { useRouter } from "vue-router";
 
-import { useDialog } from "primevue/usedialog";
-
-const dialog = useDialog();
 const props = defineProps({
   cameraFeedUrl: String,
   recognizedText: String,
   recognitionTime: String,
 });
 
+const dialog = useDialog();
 const router = useRouter();
 const data = toRef(props, "recognizedText");
-const pallet = ref({ text: "", time: "", updated: false, matching: false });
-const place = ref({ text: "", time: "", updated: false, matching: false });
-let alertModalVisible = false;
-
-function updateTagData(tagType: string, text: string) {
-  const time = new Date().toLocaleTimeString();
-  if (tagType === "pallet") {
-    pallet.value = { text, time, updated: true, matching: false }; // Ensure 'matching' is reset on each update
-    setTimeout(() => {
-      pallet.value.updated = false;
-    }, 2000); // Clear updated status after delay
-  } else if (tagType === "place") {
-    place.value = { text, time, updated: true, matching: false }; // Ensure 'matching' is reset on each update
-    setTimeout(() => {
-      place.value.updated = false;
-    }, 2000); // Clear updated status after delay
-  }
-}
-
-function checkForMatch() {
-  const palletCode = pallet.value.text.substring(2);
-  const placeCode = place.value.text.substring(2);
-
-  if (palletCode === placeCode && palletCode !== "") {
-    pallet.value.matching = true;
-    place.value.matching = true;
-  } else {
-    pallet.value.matching = false;
-    place.value.matching = false;
-  }
-}
+const pallet = ref("");
+const place = ref("");
 
 watch(
-  () => props.recognizedText,
-  (newValue) => {
-    if (!newValue) {
-      // Reset both pallet and place data when there is no recognized text
-      pallet.value = { text: "", time: "", updated: false, matching: false };
-      place.value = { text: "", time: "", updated: false, matching: false };
-      return;
+  data,
+  (oldPlace, newPlace) => {
+    newPlace = data.value;
+    if (
+      oldPlace?.charAt(0) === "S" &&
+      newPlace?.charAt(0) === "P" &&
+      compareIgnoringFirstTwo(newPlace, oldPlace)
+    ) {
+      place.value = oldPlace;
+      pallet.value = newPlace;
+      showAlertDialog();
     }
 
-    if (newValue.startsWith("P-")) {
-      if (
-        place.value.text &&
-        compareIgnoringFirstTwo(newValue, place.value.text)
-      ) {
-        showAlertDialog();
-      }
-      updateTagData("pallet", newValue);
-      if (place.value.text) {
-        checkForMatch();
-      }
-    } else if (newValue.startsWith("S-")) {
-      if (
-        pallet.value.text &&
-        compareIgnoringFirstTwo(pallet.value.text, newValue)
-      ) {
-        showAlertDialog();
-      }
-      updateTagData("place", newValue);
-      if (pallet.value.text) {
-        checkForMatch();
-      }
+    if (
+      oldPlace?.charAt(0) === "P" &&
+      newPlace?.charAt(0) === "S" &&
+      compareIgnoringFirstTwo(newPlace, oldPlace)
+    ) {
+      pallet.value = oldPlace;
+      place.value = newPlace;
+      showAlertDialog();
     }
+    oldPlace = newPlace;
   },
+  { immediate: true },
 );
 
 function compareIgnoringFirstTwo(str1: string, str2: string): boolean {
@@ -173,49 +111,43 @@ function compareIgnoringFirstTwo(str1: string, str2: string): boolean {
 }
 
 function showAlertDialog(): void {
-  if (!alertModalVisible) {
-    alertModalVisible = true;
-    dialog.open(AlertModal, {
-      props: {
-        style: {
-          width: "30vw",
-          borderRadius: "2rem",
-        },
-        breakpoints: {
-          "960px": "75vw",
-          "640px": "90vw",
-        },
-        modal: true,
-        closable: false,
-        showHeader: false,
+  dialog.open(AlertModal, {
+    props: {
+      style: {
+        width: "30vw",
+        borderRadius: "2rem",
       },
-      data: {
-        pallet: pallet.value,
-        place: place.value,
+      breakpoints: {
+        "960px": "75vw",
+        "640px": "90vw",
       },
-    });
-  }
+      modal: true,
+      closable: false,
+      showHeader: false,
+    },
+    data: {
+      pallet: pallet.value,
+      place: place.value,
+    },
+  });
 }
 
 function showMissingPalletDialog(): void {
-  if (!alertModalVisible) {
-    alertModalVisible = true;
-    dialog.open(MissingPallet, {
-      props: {
-        style: {
-          width: "30vw",
-          borderRadius: "2rem",
-        },
-        breakpoints: {
-          "960px": "75vw",
-          "640px": "90vw",
-        },
-        modal: true,
-        closable: false,
-        header: "Alert Missing Pallet",
+  dialog.open(MissingPallet, {
+    props: {
+      style: {
+        width: "30vw",
+        borderRadius: "2rem",
       },
-    });
-  }
+      breakpoints: {
+        "960px": "75vw",
+        "640px": "90vw",
+      },
+      modal: true,
+      closable: false,
+      header: "Alert Missing Pallet",
+    },
+  });
 }
 
 function logout(): void {
@@ -226,93 +158,14 @@ function logout(): void {
 
 <style scoped lang="scss">
 .main-container {
-  position: relative; // Needed for absolute positioning of children
   width: 100%;
   height: 100vh;
-  overflow: hidden; // Ensures no scroll bars appear
 }
-
 .camera-feed {
   width: 100%;
   height: 100%;
 }
 
-.info-part p {
-  margin: 0;
-  padding: 0;
-  font-weight: bold;
-}
-
-.info-part small {
-  display: block;
-  font-size: 0.75em;
-  color: #666;
-  margin-top: 5px;
-}
-.info-display {
-  position: absolute; // Positioned absolutely relative to the main container
-  top: 53%; // Center vertically
-  right: 10px; // 10px from the right edge of the main container
-  transform: translateY(-50%); // This will vertically center it at 'top: 50%'
-  width: 18%; // Adjust width as needed
-  height: auto; // Height will depend on content
-  background-color: rgba(255, 255, 255, 0.8); // Semi-transparent background
-  padding: 10px;
-  border-radius: 5px; // Optional: rounded corners
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  z-index: 100; // Make sure it's above the video feed
-}
-.info-part {
-  border: 1px solid #ccc; /* Default border color */
-  padding: 10px;
-  margin-bottom: 10px;
-}
-
-.tag-match {
-  border-color: #4caf50; /* Green border to indicate a match */
-  background-color: #e6f9e6; /* Light green background for visibility */
-  animation: flashMatch 1s ease-out;
-}
-
-@keyframes flashMatch {
-  0%,
-  100% {
-    background-color: #e6f9e6;
-  }
-  50% {
-    background-color: #c2f0c2;
-  }
-}
-.no-tags {
-  border-color: #ccc; /* Grey or neutral color indicating no tags detected */
-}
-
-// .info-part {
-//   border: 1px solid #ccc;
-//   margin-bottom: 10px;  // Space between pallet and shelf tag
-//   padding: 10px;
-//   transition: background-color 0.5s ease;
-// }
-
-// .match {
-//   animation: pulseUpdate 0.5s ease-out;
-//   border-color: #4CAF50; /* Green border to indicate update */
-// }
-
-// @keyframes pulseUpdate {
-//   0%, 100% {
-//     background-color: transparent;
-//   }
-//   50% {
-//     background-color: #d4edda;
-//   }
-// }
-
-h3 {
-  color: #333;
-}
 .leftside-buttons {
   display: flex;
   flex-direction: column;
@@ -333,14 +186,12 @@ h3 {
   width: 5rem;
   height: 5rem;
   margin-bottom: 1rem;
-  background-color: white;
 }
 
 .alert-button {
   width: 5rem;
   height: 5rem;
   margin-bottom: 1rem;
-  background-color: white;
 }
 
 .pallet-icon {
@@ -371,129 +222,3 @@ h3 {
   }
 }
 </style>
-
-<!--
-  
- // watch(
-//   data,
-//   (oldPlace, newPlace) => {
-//     if (
-//       oldPlace?.charAt(0) === "S" &&
-//       newPlace?.charAt(0) === "P" &&
-//       compareIgnoringFirstTwo(newPlace, oldPlace)
-//     ) {
-//       place.value = oldPlace;
-//       pallet.value = newPlace;
-//       showAlertDialog();
-//     }
-
-//     if (
-//       oldPlace?.charAt(0) === "P" &&
-//       newPlace?.charAt(0) === "S" &&
-//       compareIgnoringFirstTwo(newPlace, oldPlace)
-//     ) {
-//       pallet.value = oldPlace;
-//       place.value = newPlace;
-//       showAlertDialog();
-//     }
-//   },
-//   { immediate: true },
-// ); 
-  
-  
-  <style scoped lang="scss">
-.main-container {
-  width: 100%;
-  height: 100vh;
-}
-.camera-feed {
-  width: 100%;
-  height: 100%;
-}
-
-.leftside-buttons {
-  display: flex;
-  flex-direction: column;
-  position: absolute;
-  top: 2%;
-  left: 2%;
-}
-
-.buttons {
-  display: flex;
-  flex-direction: column;
-  position: absolute;
-  top: 2%;
-  right: 2%;
-}
-
-.table-button {
-  width: 5rem;
-  height: 5rem;
-  margin-bottom: 1rem;
-  background-color: white;
-}
-
-.alert-button {
-  width: 5rem;
-  height: 5rem;
-  margin-bottom: 1rem;
-  background-color: white;
-}
-
-.pallet-icon {
-  padding-top: 0.5rem;
-  width: 5rem;
-  height: 5rem;
-  color: white;
-}
-
-.alert-icon {
-  width: 3rem;
-  height: 3rem;
-  color: white;
-}
-
-.alertTest {
-  position: absolute;
-  top: 0%;
-  left: 0%;
-  width: 5rem;
-  height: 5rem;
-}
-
-.alert-button {
-  :deep(.pi) {
-    font-size: 2rem;
-    color: black;
-  }
-}
-
-.info-display {
-  width: 80%;
-  display: flex;
-  justify-content: space-around;
-  margin-top: 20px;
-}
-
-.info-part {
-  border: 2px solid #ccc;
-  padding: 10px;
-  margin: 5px;
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: #f5f5f5; /* Default light gray background */
-}
-
-.match {
-  background-color: #4CAF50; /* Green background on match */
-  border-color: #4CAF50; /* Optional: change border color on match */
-}
-
-h3 {
-  margin-top: 0;
-  color: #333;
-}
-</style> -->
